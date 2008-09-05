876bd33c9eb05b1d388713fea460051572cfc33c
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2008-09-05 07:43:28 +0000

    Chatting is detected, although nothing is done with it. BTW, the main loop needs to update the GUI every loop or something because it's set to ignorerepaint which is useful but requires finer control.

--- src/ucbang/gui/ClientGUI.java
+++ src/ucbang/gui/ClientGUI.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferStrategy;
@@ -16,21 +18,21 @@ import javax.swing.JOptionPane;
 import ucbang.core.Card;
 import ucbang.core.Player;
 
-public class ClientGUI extends JFrame{
-	Keyboard kb;
+public class ClientGUI extends JFrame implements KeyListener{
 	BufferStrategy strategy;
     int player;
-    
+    StringBuilder chat;
+    boolean chatting=false;
     public ClientGUI() {
     }
     public ClientGUI(int p) {
         player = p;
+        chat=new StringBuilder();
         //set window sizes
         setPreferredSize(new Dimension(800,600));
         setSize(new Dimension(800,600));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        kb=new Keyboard();
-        addKeyListener(kb);
+        addKeyListener(this);
      	this.setIgnoreRepaint(true);
      	this.setVisible(true);
     	this.createBufferStrategy(2);
@@ -49,6 +51,10 @@ public class ClientGUI extends JFrame{
 		graphics.fillRect(0, 0, 800, 400);
 		graphics.setColor(new Color(100,0,0));
 		graphics.fillRect(0, 400, 800, 600);
+		if(chatting){
+			graphics.setColor(Color.WHITE);
+			graphics.drawString(chat.toString(), 10, 550);
+		}
 
 		graphics.dispose();
 		//paint backbuffer to window
@@ -81,4 +87,29 @@ public class ClientGUI extends JFrame{
 
         return n;
     }
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		if(e.getKeyChar()=='\n'){
+			chatting=!chatting;
+			if(!chatting&&chat.length()>0){
+				System.out.println("Sent chat message "+chat);
+				chat.delete(0, chat.length());
+				//TODO: Actually send chats.
+			}
+		}else if(chatting){
+			chat.append(e.getKeyChar());
+		}
+	}
 }
