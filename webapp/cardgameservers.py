02845b70f4efea734e21d76662340757b7894d72
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2008-09-22 20:45:57 +0000

    Storing the fields in the database, now I just need to check the SHA-1 hash and the server side code should be more or less set.

--- webapp/cardgameservers.py
+++ webapp/cardgameservers.py
@@ -1,17 +1,69 @@
+import cgi
+
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
 
+class Server(db.Model):
+    ip = db.StringProperty()
+    gamename = db.StringProperty()
+    type = db.StringProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
+    
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
-        
+        self.response.out.write('<html><body>')
+    
+        servers = db.GqlQuery("SELECT * FROM Server ORDER BY date DESC LIMIT 10")
+    
+        for server in servers:
+            self.response.out.write('<p>Name: %s'
+                                    % cgi.escape(server.gamename))
+    
+        # Write the submission form and the footer of the page
+        self.response.out.write("""
+            </body>
+          </html>""")
+    
+class AddServer(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write("""
+      <html>
+        <head>
+        <title>Card Game Servers - Add Server</title>
+        </head>
+        <body>
+          <form action="/sign" method="post">
+            <div>
+                Name: <input type="text" name="gamename"></input><br />
+                IP Address: <input type="text" name="ip"></input><br />
+                Game Type: <input type="text" name="type"></input><br />
+                SHA-1 Hash: <input type="text" name="hash"></input><br />
+            </div>
+            <div><input type="submit" value="Add Server"></div>
+          </form>
+        </body>
+      </html>""")
+
+
+class ServerList(webapp.RequestHandler):
+  def post(self):
+      server = Server()
+      server.ip = self.request.get('ip')
+      server.gamename = self.request.get('gamename')
+      server.type = self.request.get('type')
+      server.put()
+      self.redirect('/')
+
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [('/', MainPage),
+                                      ('/add', AddServer),
+                                      ('/sign', ServerList)],
                                      debug=True)
 
 def main():
-    run_wsgi_app(application)
-    
+  run_wsgi_app(application)
+
 if __name__ == "__main__":
-    main()
+  main()
