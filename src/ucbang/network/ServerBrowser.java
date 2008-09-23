58ff972e4690dc58b798c138eb732e851ff7f5ba
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2008-09-23 18:09:28 +0000

    Server browser parses XML correctly, and I'm late to CS lab.

--- src/ucbang/network/ServerBrowser.java
+++ src/ucbang/network/ServerBrowser.java
@@ -15,13 +15,30 @@ import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.DocumentType;
 import org.w3c.dom.Entity;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 public class ServerBrowser {
+	private class ServerInfo{
+		String name;
+		String ip;
+		String type;
+		public ServerInfo(String name, String ip, String type){
+			this.name=name;
+			this.ip=ip;
+			this.type=type;
+		}
+		public String toString(){
+			return type+" server \""+name+"\"on "+ip;
+		}
+	}
+	ArrayList<ServerInfo> servers = new ArrayList<ServerInfo>();
 	public static void main (String args[]){
 		new ServerBrowser().downloadList();
 	}
@@ -37,16 +54,38 @@ public class ServerBrowser {
 		        dbf.setIgnoringElementContentWhitespace(true);
 	        	DocumentBuilder db = dbf.newDocumentBuilder();
 		        Document doc = db.parse(hConnection.getInputStream());
+		        for(Node server=doc.getFirstChild().getChildNodes().item(1);
+		        	server!=null;server=server.getNextSibling()){
+		        	if(server.getNodeName().equals("server")){
+		        		servers.add(processServerNode(server));
+		        	}
+		        }
 				/*BufferedReader is = new BufferedReader(new InputStreamReader(hConnection.getInputStream()));
 				while(is.ready())
 				System.out.println(is.readLine());*/
 				hConnection.disconnect();
+				Iterator<ServerInfo> iter = servers.iterator();
+				while(iter.hasNext()){
+					System.out.println(iter.next());
+				}
 			}else{
 				System.out.println("Ugh, something bad happened");
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	ServerInfo processServerNode(Node n){
+		NodeList ns = n.getChildNodes();
+		return new ServerInfo(ns.item(1).getTextContent(),ns.item(3).getTextContent(),ns.item(5).getTextContent());
+	}
+	void recPrint(Node n){
+		if(!n.getNodeName().equals("#text"))
+			System.out.println(n.getNodeName()+":"+n.getTextContent());
+		NodeList ns = n.getChildNodes();
+		for(Node a = ns.item(0);a!=null;a=a.getNextSibling()){
+			recPrint(a);
 		}
 	}
     private static class MyErrorHandler implements ErrorHandler {
