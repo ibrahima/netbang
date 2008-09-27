9e85189c8fef4508d4879f866793376579efc8a7
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2008-09-27 08:58:12 +0000

    Some more descriptions but they really kind of fail so someone should just write them.

--- src/ucbang/core/Card.java
+++ src/ucbang/core/Card.java
@@ -80,12 +80,17 @@ public class Card {
 				target = 2;
 				range = 1;
 				effect = play.DAMAGE.ordinal();
+				description="The current player play a Knife card in front of him. Starting with the next\n" +
+						"player's turn, he can discard it for BANG! effect. This card is not count as BANG!";
 				break;
 			case BUFFALO_RIFLE:
 				type = 3;
 				target = 2;
 				range = -1;
 				effect = play.DAMAGE.ordinal();
+				description="The current player play a Buffalo Rifle card in front of him. Starting with the\n" +
+						"next player's turn, he can discard it for BANG! effect to 1 player at every distance.\n" +
+						"This card is not count as BANG!";
 				break;
 			case SPRINGFIELD:
 				type = 2;
@@ -93,12 +98,17 @@ public class Card {
 				discardToPlay = true;
 				range = -1;
 				effect = play.DAMAGE.ordinal();
+				description="Player which is on turn, discard card Springfield together with another card on the\n" +
+						"deck. Than he choose one player, which is target of attack with BANG! effect.";
 				break;
 			case PEPPERBOX:
 				type = 3;
 				target = 2;
 				range = 1;
 				effect = play.DAMAGE.ordinal();
+				description="The current player play a PepperBox card in front of him. Starting with the next\n" +
+						" player's turn, he can discard it for BANG! effect to 1 player at visible distance.\n" +
+						"This card is not count as BANG!.";
 				break; // TODO: make same range as bang
 			case DERRINGER:
 				type = 3;
@@ -106,12 +116,20 @@ public class Card {
 				range = 1;
 				effect = play.DAMAGE.ordinal();
 				effect2 = play.DRAW.ordinal();
+				description="The current player plays a Derringer card in front of her. During one of her following\n" +
+						"turns, provided she still has the card in front of her, she can choose to discard it to a BANG!\n" +
+						"on a player at a distance of 1, and also draw a card from the deck. ";
 				break;
 			case DUEL:
 				type = 2;
 				target = 2;
 				range = -1;
 				effect = play.DUEL.ordinal();
+				description="The player playing this card challenges any other player (at any distance), staring him in\n" +
+						"the eyes. The challenged player may discard a BANG! card (even though it is not his turn!).\n" +
+						"If he does, the player who played the Duel card may discard a BANG! card, and so on: the first\n" +
+						"player failing to play a BANG! card loses one life point, and the duel is over. Note: you cannot\n" +
+						"use the Barrel or play Missed! cards during a duel, and the Duel is not considered a BANG! card.";
 				break;
 			case DYNAMITE:
 				type = 3;
