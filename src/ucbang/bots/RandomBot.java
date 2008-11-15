cca124713341928b014c59699d48155747296d2a
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2008-11-15 20:51:05 +0000

    Randombot actually randoms now.

--- src/ucbang/bots/RandomBot.java
+++ src/ucbang/bots/RandomBot.java
@@ -1,7 +1,18 @@
 package ucbang.bots;
 
+import java.util.Random;
+
+/**
+ * The Randombot is a bot that randomly responds to prompts to play a card.
+ * @author Ibrahim
+ *
+ */
 public class RandomBot extends Bot {
 
+	/**
+	 * @param host
+	 * @param name
+	 */
 	public RandomBot(String host, String name) {
 		super(host, name);
 	}
@@ -13,7 +24,9 @@ public class RandomBot extends Bot {
 	 * Prompts the player to play a card
 	 */
 	protected void promptPlayCard() {
-		outMsgs.add("Prompt:0");
+		Random r = new Random();
+		int choice =  r.nextInt(this.player.hand.size());
+		outMsgs.add("Prompt:"+choice);
 	}
 
 }
