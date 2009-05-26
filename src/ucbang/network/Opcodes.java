3b64728b244ac5bf84ff947841b8fa1750c685c3
Alan Wang <asw12@berkeley.edu>
2009-05-26 06:51:00 +0000

    New class of static methods and enums to handle the client/server message passing.

---
+++ src/ucbang/network/Opcodes.java
@@ -0,0 +1,28 @@
+package ucbang.network;
+
+public class Opcodes {
+    enum Message{
+        SETNAME(0),
+        COMMAND(0),
+        READY(0);
+        
+        private Message(int i){
+            options = i;
+        }
+        int options;
+        public int numOptions(){
+            return options;
+        }
+    }
+    
+    enum Commands{
+        QUIT,
+        CLOSESERVER,
+        START,
+        PROMPTING;
+    }
+    
+    public static Message fromString(String str){
+        return Message.valueOf(str);
+    }
+}
