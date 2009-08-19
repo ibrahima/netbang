0be8204bd36a766672965a3e10146b077e49219b
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2009-08-19 11:57:31 -0700

    Add a string to the Constants class for discarding stuff. I want to add all strings to this eventually.

--- src/netbang/core/Constants.java
+++ src/netbang/core/Constants.java
@@ -5,4 +5,12 @@ public class Constants {
     }
 
     public static enum Role {SHERIFF, DEPUTY, OUTLAW, RENEGADE};
+    /**
+     * The following constants describe the netbang message protocol.
+     */
+    /**
+     * This string is sent when a player wants to discard a card.
+     */
+    public final String DISCARD = "discard";
+    
 }
