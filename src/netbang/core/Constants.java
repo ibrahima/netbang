38edfc9b2948d2d9485d529b9a88b88944156874
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2009-08-19 18:02:13 -0700

    Move protocol stuff to its own class

--- src/netbang/core/Constants.java
+++ src/netbang/core/Constants.java
@@ -5,12 +5,4 @@ public class Constants {
     }
 
     public static enum Role {SHERIFF, DEPUTY, OUTLAW, RENEGADE};
-    /**
-     * The following constants describe the netbang message protocol.
-     */
-    /**
-     * This string is sent when a player wants to discard a card.
-     */
-    public final String DISCARD = "discard";
-    
 }
---
+++ src/netbang/network/Protocol.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package netbang.network;
+
+/**
+ * @author Ibrahim
+ *	This class holds constants that describe the netbang network protocol.
+ */
+public class Protocol {
+
+    /**
+     * This string is sent when a player wants to discard a card.
+     */
+    public final String DISCARD = "discard";
+}
